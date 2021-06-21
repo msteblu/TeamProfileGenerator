d6ed0e96d50bd943e5529a3a2fcaa9d6583f09d2
Megan Steblay <megan@steblay.net>
2021-06-21 14:35:16 -0500

    Updating so that the file will write HTML pages

--- index.js
+++ index.js
@@ -1,139 +1,175 @@
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const template = require("./src/template");
 
-// Initialize a new Employee object
-const employee = new Employee();
+let manager = {};
+let engineer = {};
+let intern = {};
 
-// Start playing
-// game.play();
-
-
-// // Include packages needed for this application
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-// // Link to the generateMarkdown file, which provides the badge/link for licenses:
-// const generate = require('./utils/generateMarkdown.js');
-
-// // Prompts for users to collect their input: 
-// const promptUser = () => {
-//     return inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: 'title',
-//             message: 'What is the title of your project?',
-//         },
-//         {
-//             type: 'input',
-//             name: 'description',
-//             message: 'Provide a short description explaining the what, why, and how of your project.',
-//         },
-//         {
-//             type: 'input',
-//             name: 'installation',
-//             message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
-//         },
-//         {
-//             type: 'input',
-//             name: 'usage',
-//             message: 'Provide instructions and examples for use.',
-//         },
-//         {
-//             type: 'checkbox',
-//             name: 'license',
-//             message: 'Please choose a license for your project. (Listed from most conditions to none.)',
-//             choices: [
-//                 'GNU AGPLv3',
-//                 'GNU GPLv3',
-//                 'GNU LGPLv3',
-//                 'Mozilla Public License 2.0',
-//                 'Apache License 2.0',
-//                 'MIT License',
-//                 'Boost Software License 1.0',
-//                 'The Unlicense',
-//             ],
-//         },
-//         {
-//             type: 'input',
-//             name: 'contributing',
-//             message: 'Provide guidelines for how other developers can contribute to your project.',
-//         },
-//         {
-//             type: 'input',
-//             name: 'tests',
-//             message: 'Provide examples on how to run tests for your application.',
-//         },
-//         {
-//             type: 'input',
-//             name: 'username',
-//             message: 'What is your GitHub username?',
-//         },
-//         {
-//             type: 'input',
-//             name: 'email',
-//             message: 'What is your email address?',
-//         },
-//     ])
-// };
+let engineers = [];
+let interns = [];
 
 
-// // Function to write README file
-// const writeToFile = (data, badge, link) =>
-//     `# ${data.title}
+// Prompts for users to collect their input: 
+const menu = () => {
+    return inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'choices',
+            message: `Add another team member?`,
+            choices: [
+                'Engineer',
+                'Intern',
+                'Quit'
+            ]
+        }
+    ])
+};
 
-// ${badge}
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerID',
+            message: `What is the team manager's employee ID?`,
+        },
+        {
+            type: 'input',
+            name: 'managerName',
+            message: `What is the team manager's name?`,
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: `What is the team manager's email?`,
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: `What is the team manager's office number?`,
+        },
+    ])
+};
 
-// ## Description
+const promptEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: `What is the engineer's employee ID?`,
+        },
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: `What is the engineer's name?`,
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: `What is the engineer's email?`,
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: `What is the engineer's GitHub username?`,
+        },
+    ])
+};
 
-// ${data.description}
-    
-// ## Table of Contents
-//  - [Installation](#installation)
-//  - [Usage](#usage)
-//  - [License](#license)
-//  - [Contributing](#contributing)
-//  - [Tests](#tests)
-//  - [Questions](#questions)
-    
-    
-// ## Installation
+const promptIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internID',
+            message: `What is the intern's employee ID?`,
+        },
+        {
+            type: 'input',
+            name: 'internName',
+            message: `What is the intern's name?`,
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: `What is the intern's email?`,
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: `What is the intern's school?`,
+        },
+    ])
+};
 
-// ${data.installation}
-    
-// ## Usage
 
-// ${data.usage}
-    
-// ## License
+const inquireManager = () => {
+    promptManager()
+        .then((data) => {
+            manager = new Manager(
+                data.managerID,
+                data.managerName,
+                data.managerEmail,
+                data.officeNumber
+            );
 
-// This project is licensed under [${data.license}](${link}).
-    
-// ## Contributing
+            console.log(manager.getName());
+            inquireChoice();
+        });
+};
 
-// ${data.contributing}
-    
-// ## Tests
+const inquireEngineer = () => {
+    promptEngineer()
+        .then((data) => {
+            engineer = new Engineer(
+                data.engineerID,
+                data.engineerName,
+                data.engineerEmail,
+                data.github
+            );
+            engineers.push(engineer);
+            console.log(engineer.getName());
+            inquireChoice();
+        });
+};
 
-// ${data.tests}
-    
-// ## Questions
+const inquireIntern = () => {
+    promptIntern()
+        .then((data) => {
+            intern = new Intern( 
+                data.internID,
+                data.internName,
+                data.internEmail,
+                data.school
+            );
+            interns.push(intern); 
+            console.log(intern.getName());
+            inquireChoice();
+        });
+};
 
-// See more of my work on my [GitHub Profile](https://github.com/${data.username}/).
-// For any additional questions, reach me at my email: ${data.email}.`;
+const inquireChoice = () => {
+    menu()
+        .then((data) => {
+            switch (data.choices[0]) {
+                case "Engineer":
+                    inquireEngineer();
+                    break;
+                case "Intern":
+                    inquireIntern();
+                    break;
+                case "Quit":
+                default:
+                    console.log(manager, engineers, interns);
+                    template.createFile(manager, engineers, interns);
+                    break;
+            }
+        });
+};
 
-// // Function to initialize app
-// // Run the prompts, then pass in the collected data and the correct license badges/links
-// // Write out a new README file
-// const init = () => {
-//     promptUser()
-//         .then((data) => {
-//             let { badge, link } = generate.renderLicense(data.license);
-//             const readmePageContent = writeToFile(data, badge, link);
-
-//             fs.writeFile('README.md', readmePageContent, (err) =>
-//                 err ? console.log(err) : console.log('Successfully wrote to README.md')
-//             );
-//         })
-
-// };
-
-// // Function call to initialize app
-// init();
+inquireManager();
---
+++ src/template.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+
+let fileContent = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid bg-success text-light py-3 mb-4">
+        <div class="container text-center">
+            <h1 class="display-4">My Team</h1>
+        </div>
+    </div>
+    <main>
+
+        <div class="card-columns">
+            <div class="row justify-content-center">
+    `; 
+
+let fileEnding = `          </div>
+        </div>
+
+    </main>
+</body>
+
+</html>`;
+
+const createManagerCard = (manager) => {
+    console.log(manager);
+  return `              <div class="card border-dark m-3 bg-warning" style="max-width: 18rem;">
+                    <div class="text-center">
+                        <div class="card-header h1"><strong>${manager.name}</strong></div>
+                        <div class="card-header h2"><em>${manager.getRole()}</em></div>
+                    </div>
+                    <div class="card-body text-left text-dark m-2 bg-light" style="border: 2px dotted gray">
+                        <p class="card-text"><strong>ID:</strong> ${manager.id}</p>
+                        <p class="card-text"><strong>Email: </strong><a href = "mailto: ${manager.email}" class="link-success">${manager.email}</a></p>
+                        <p class="card-text"><strong>Office Number: </strong>${manager.officeNumber}</p>
+                    </div>
+                </div>
+    `;
+};
+
+const createEngineerCard = (engineer) => {
+    console.log(engineer);
+  return `              <div class="card border-dark m-3 bg-warning" style="max-width: 18rem;">
+                    <div class="text-center">
+                        <div class="card-header h1"><strong>${engineer.name}</strong></div>
+                        <div class="card-header h2"><em>${engineer.getRole()}</em></div>
+                    </div>
+                    <div class="card-body text-left text-dark m-2 bg-light" style="border: 2px dotted gray">
+                        <p class="card-text"><strong>ID: </strong>${engineer.id}</p>
+                        <p class="card-text"><strong>Email: </strong><a href = "mailto: ${engineer.email}" class="link-success">${engineer.email}</a></p>
+                        <p class="card-text"><strong>GitHub Profile: </strong><a href="https://www.github.com/${engineer.github}" target="_blank" rel="noopener noreferrer" class="link-success">${engineer.github}</a></p>
+                    </div>
+                </div>
+    `;
+};
+
+const createInternCard = (intern) => {
+  return `                 <div class="card border-dark m-3 bg-warning" style="max-width: 18rem;">
+                    <div class="text-center">
+                        <div class="card-header h1"><strong>${intern.name}</strong></div>
+                        <div class="card-header h2"><em>${intern.getRole()}</em></div>
+                    </div>
+                    <div class="card-body text-left text-dark m-2 bg-light" style="border: 2px dotted gray">
+                        <p class="card-text"><strong>ID: </strong>${intern.id}</p>
+                        <p class="card-text"><strong>Email: </strong><a href = "mailto: ${intern.email}" class="link-success">${intern.email}</a></p>
+                        <p class="card-text"><strong>School: </strong>${intern.school}</p>
+                    </div>
+                </div>
+    `;
+};
+
+const createFile = (manager, engineers, interns) => {
+  fileContent = fileContent + createManagerCard(manager);
+  engineers.forEach(iterateEngineers);
+  interns.forEach(iterateInterns);
+  createFullFile();
+};
+
+const createFullFile = () => {
+    fileContent = fileContent +fileEnding;
+    writeHTML();
+};
+
+const iterateEngineers = (value) => {
+  // fileContent.concat(createEngineerCard(value));
+  fileContent = fileContent + createEngineerCard(value);
+};
+
+const iterateInterns = (value) => {
+  // fileContent.concat(createInternCard(value));
+  fileContent = fileContent + createInternCard(value);
+};
+
+const writeHTML = function () {
+  fs.writeFile(`dist/Test.html`, fileContent, (err) =>
+                err ? console.log(err) : console.log('Successfully wrote to Test.txt')
+            );
+}
+
+module.exports = {
+    createManagerCard,
+    createEngineerCard,
+    createInternCard,
+    createFile,
+    iterateEngineers,
+    iterateInterns,
+    writeHTML,
+}
