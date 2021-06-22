bd432eb271a79ce4230bfd454b7a45d853395339
Megan Steblay <megan@steblay.net>
2021-06-21 21:39:50 -0500

    Updating tests for Intern

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,31 +1,105 @@
 const Intern = require("../lib/Intern");
-const Employee = require("../lib/Employee");
 
 describe("Intern class", () => {
   describe("Initialization", () => {
-    it("Employee id is equal to what is entered", () => {
-      const obj = new Employee();
-      expect("id" in obj).toBe();
+    it("Intern object was created correctly", () => {
+      // Arange
+      let id = "3";
+      let name = "Name"
+      let email = "name@email.com"
+      const obj = new Intern(id, name, email);
+      // Assert
+      expect(obj.id).toEqual("3");
+      expect(obj.name).toEqual("Name");
+      expect(obj.email).toEqual("name@email.com");
     });
+    // Exception test of id
+    it("Should throw an error if not provided text for id", () => {
+      // Arrange
+      const err = new Error("Expected parameter 'id' to be a non-empty string");
+      const cb = () => new Intern("", "Name", "name@email.com");
+      // Assert
+      expect(cb).toThrowError(err);
+    });
+    // Exception test of name
+    it("Should throw an error if not provided text for name", () => {
+      // Arrange
+      const err = new Error("Expected parameter 'name' to be a non-empty string");
+      const cb = () => new Intern("3", "", "name@email.com");
+      // Assert
+      expect(cb).toThrowError(err);
+    });
+    // Exception test of email
+    it("Should throw an error if not provided text for email", () => {
+      // Arrange
+      const err = new Error("Expected parameter 'email' to be a non-empty string");
+      const cb = () => new Intern("3", "Name", "");
+      // Assert
+      expect(cb).toThrowError(err);
+    });    
+    it("Should throw an error if email address is not formatted correctly", () => {
+      // Arrange
+      const err = new Error("Expected parameter 'email' to be in standard email address format");
+      const cb = () => new Intern("3", "Name", "email");
+      // Assert
+      expect(cb).toThrowError(err);
+    });        
   });
   
-  describe("ID", () => {
+  describe("Get ID", () => {
+    it("getID method should return correct id", () => {
+    // Arrange
+    const obj = new Intern("3", "Name", "name@email.com");
+    // Act
+    const id = obj.getId();
+    // Assert
+    expect(id).toEqual(obj.id);
+    });
 
   });
 
-  describe("Name", () => {
-
+  describe("Get Name", () => {
+    it("getName method should return correct name", () => {
+      // Arrange
+      const obj = new Intern("3", "Name", "name@email.com");
+      // Act
+      const name = obj.getName();
+      // Assert
+      expect(name).toEqual(obj.name);
+      });
+  
   });
 
-  describe("Email", () => {
-
+  describe("Get Email", () => {
+    it("getEmail method should return correct email", () => {
+      // Arrange
+      const obj = new Intern("3", "Name", "name@email.com");
+      // Act
+      const email = obj.getEmail();
+      // Assert
+      expect(email).toEqual(obj.email);
+      });
   });
 
-  describe("Role", () => {
-
+  describe("Get Role", () => {
+    it("getRole method should return correct role", () => {
+      // Arrange
+      const obj = new Intern("3", "Name", "name@email.com");
+      // Act
+      const role = obj.getRole();
+      // Assert
+      expect(role).toEqual("Intern");
+      });
   });
 
-  describe("School", () => {
-
+  describe("Get School", () => {
+    it("getSchool method should return correct GitHub profile", () => {
+      // Arrange
+      const obj = new Intern("3", "Name", "name@email.com");
+      // Act
+      const role = obj.getSchool();
+      // Assert
+      expect(role).toEqual(obj.school);
+      });
   });
 });
